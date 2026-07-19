cbc12623d54a03665ab503499fda66f08b677225
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-07-19 16:47:47 +0600

    delete appointment

--- appointment_management/views.py
+++ appointment_management/views.py
@@ -50,8 +50,10 @@ class AppointmentDetailViewAPI(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-
-
+    def delete(self, request, pk):
+        appointment = get_object_or_404(Appointment, pk=pk)
+        appointment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
     

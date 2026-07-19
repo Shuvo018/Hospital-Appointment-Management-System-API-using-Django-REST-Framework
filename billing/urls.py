dc3b578bd665550fe55036839f28ab0734018173
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-07-19 19:57:46 +0600

    feat: add BillDetailsViewAPI for managing bill details and update URLs

--- billing/urls.py
+++ billing/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from billing.views import DoctorBillViewAPI,BillViewAPI
+from billing.views import DoctorBillViewAPI,BillViewAPI, BillDetailsViewAPI
 
 # bills/
 urlpatterns = [
     path('', BillViewAPI.as_view()),
     path('doctor-bills/', DoctorBillViewAPI.as_view()),
+
+    path('<int:pk>/', BillDetailsViewAPI.as_view()),
 
 ]
--- billing/views.py
+++ billing/views.py
@@ -35,4 +35,34 @@ class BillViewAPI(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
     
+
+class BillDetailsViewAPI(APIView):
+    
+    def get(self, request, pk):
+        bill = get_object_or_404(Bill, pk=pk)
+        serializer = BillSerializer(bill)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        bill = get_object_or_404(Bill, pk=pk)
+        serializer = BillSerializer(bill,request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, pk):
+        bill = get_object_or_404(Bill, pk=pk)
+        serializer = BillSerializer(bill,request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        bill = get_object_or_404(Bill, pk=pk)
+        bill.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)        
+
+        
     

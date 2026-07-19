03643bb16d6104ebc3b598821e229e84ab3ea3ac
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-07-19 16:47:30 +0600

    update appointment status/details

--- appointment_management/views.py
+++ appointment_management/views.py
@@ -42,6 +42,14 @@ class AppointmentDetailViewAPI(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    def patch(self, request, pk):
+        appointment = get_object_or_404(Appointment, pk=pk)
+        serializer = AppointmentSerializer(appointment, data=request.dta, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 
 
 
